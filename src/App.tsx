9af6636ab80c8661b90dcc7ee38bd07eb20ef902
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-10-17 14:58:52 +0300

    Fix path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { FunctionComponent } from "react";
 import { Routes, Route } from "react-router-dom";
+import { REACT_ROUTES } from "./constans/routes";
 import { Home, Todos, Users } from "./screens";
 
 const App: FunctionComponent = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/todos" element={<Todos />} />
+      <Route path={REACT_ROUTES.HOME} element={<Home />} />
+      <Route path={REACT_ROUTES.USERS} element={<Users />} />
+      <Route path={REACT_ROUTES.TODOS} element={<Todos />} />
     </Routes>
   );
 }
